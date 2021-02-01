b526f0a6cb2c68863c40150f57acc7647ca9a0e8
de.toolcreator <de.toolcreator@gmail.com>
2021-02-01 13:46:27 +0100

    Add CLI

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,19 +11,20 @@ import (
 )
 
 func main() {
-	port := 8080
-	updateInterval := 1000
+	port := flag.Int("port", 8080, "The port godstatsdog listens on")
+	updateInterval := flag.Int("interval", 1000, "The update interval in milliseconds")
+	flag.Parse()
 
-	statsUpdateTicker := time.NewTicker(time.Duration(updateInterval) * time.Millisecond)
+	statsUpdateTicker := time.NewTicker(time.Duration(*updateInterval) * time.Millisecond)
 	go updateMetrics(statsUpdateTicker)
 
 	http.Handle("/metrics", promhttp.Handler())
 	err := make(chan error)
 	go func() {
-		err <- http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+		err <- http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
 	}()
 
-	log.Println(fmt.Sprintf("Listening on port %d.", port))
+	log.Println(fmt.Sprintf("Listening on port %d.", *port))
 	log.Println("Metrics are exposed at /metrics endpoint.")
 
 	<-err
